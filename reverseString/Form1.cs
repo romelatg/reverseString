ad67fe487ddd56d0f46bb943cde0aaafa060ab81
Alexis <romelitis@hotmail.com>
2023-06-21 15:54:13 -0600

    Add project files.

---
+++ reverseString/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reverseString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String textWord = textBox1.Text;
+            string newword = "";
+            for (int i = textWord.Length - 1; i >= 0; i--)
+            {
+                newword += textWord[i];
+            }
+            label1.Text = newword;
+        }
+    }
+}
